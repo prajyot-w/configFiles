d2c43a31a319122b5a20f962903f9c16a282e0f9
Prajyot Walali <prajyot.walali@techbulls.com>
2020-11-11 18:16:38 +0530

    added files to configure git user and email on local repositories

---
+++ scripts/git-dipali-mode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "----# Current Git Credentials #----";
+git config user.name;
+git config user.email;
+
+git config user.name "Dipali Patil";
+git config user.email "dipali.patil@techbulls.com";
+
+echo "----# Modified Git Credentials #---";
+git config user.name;
+git config user.email;
